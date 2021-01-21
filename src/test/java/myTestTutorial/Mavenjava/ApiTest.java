1b968b0f2f6794af2eaa0c400ec60c10703ea991
unknown <marcin1291299@gmail.com>
2021-01-21 23:14:49 +0100

    first commit

---
+++ src/test/java/myTestTutorial/Mavenjava/ApiTest.java
@@ -0,0 +1,17 @@
+package myTestTutorial.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class ApiTest {
+	@Test
+	public void JiraTest()
+	{
+		System.out.println("JiraTest");
+	}
+	@Test
+	public void TwitterTest()
+	{
+		System.out.println("TwitterTest");
+	}
+	
+}
---
+++ src/test/java/myTestTutorial/Mavenjava/AppiumTest.java
@@ -0,0 +1,16 @@
+package myTestTutorial.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void Test1()
+	{
+		System.out.println("AppiuferfsemTe");
+	}
+	@Test
+	public void Test2()
+	{
+		System.out.println("AppiumTest2");
+	}
+}
